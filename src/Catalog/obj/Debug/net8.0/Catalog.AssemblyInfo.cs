4fd8120dc112dfd034c7665e0b04731505678331
LuisFelipePoma <u202110902@upc.edu.pe>
2023-12-29 10:46:58 -0500

    Catalog -> Dependency Injection, DTOSs

--- src/Catalog/obj/Debug/net8.0/Catalog.AssemblyInfo.cs
+++ src/Catalog/obj/Debug/net8.0/Catalog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca43f419d77170bdfea8f7ad325eec13ce2ecb20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61a7c5870ae000cf19b039b9e94ff640cbd915a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
