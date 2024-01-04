4a375d6431778f53346a2a376aae97c59d93d4eb
LuisFelipePoma <u202110902@upc.edu.pe>
2024-01-04 09:56:39 -0500

    Catalog -> Add REST with Mongo, Docker Mongo, extensions, config File, persists data

--- src/Catalog/Program.cs
+++ src/Catalog/Program.cs
@@ -1,10 +1,22 @@
 using Catalog.Repositories;
+using Catalog.Settings;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+builder.Services.AddSingleton<IMongoClient>(ServiceProvider =>
+{
+	var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+	return new MongoClient(settings.ConnectionString);
+});
+builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
---
+++ src/Catalog/Settings/MongoDbsettings.cs
@@ -0,0 +1,9 @@
+namespace Catalog.Settings
+{
+	public class MongoDbSettings
+	{
+		public string Host { get; set; }
+		public int Port { get; set; }
+		public string ConnectionString => $"mongodb://{Host}:{Port}";
+	}
+}
