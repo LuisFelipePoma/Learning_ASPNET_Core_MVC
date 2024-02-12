de1027d7ca9f382e2d2fee58bb0b0e608ae24256
Felipe <u202110902@upc.edu.pe>
2024-02-12 10:33:18 -0500

    testing

--- src/Orelly/Building_Web_ASPNET/MyBGList/Program.cs
+++ src/Orelly/Building_Web_ASPNET/MyBGList/Program.cs
@@ -3,32 +3,58 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 // This add the OpenAPI document to the service collection
-// We’re telling Swagger to resolve all conflicts related to duplicate routing handlers by always taking the first one found and ignoring the others. 
-builder.Services.AddSwaggerGen(opts =>
-	opts.ResolveConflictingActions(apiDesc => apiDesc.First())
-	);
+// We’re telling Swagger to resolve all conflicts related to duplicate routing handlers by always taking the first one found and ignoring the others.
+builder.Services.AddSwaggerGen(opts => opts.ResolveConflictingActions(apiDesc => apiDesc.First()));
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(cfg =>
+    {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+    options.AddPolicy(
+        name: "AnyOrigin",
+        cfg =>
+        {
+            cfg.AllowAnyOrigin();
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+        }
+    );
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Configuration.GetValue<bool>("UseSwagger"))
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
-	app.UseDeveloperExceptionPage();
+    app.UseDeveloperExceptionPage();
 else
-	app.UseExceptionHandler("/error");
-
+    app.UseExceptionHandler("/error");
 
 app.UseHttpsRedirection();
+
+// Enable CORS
+app.UseCors("AnyOrigin");
+
 // Minimal API
 app.MapGet("/error", () => Results.Problem());
-app.MapGet("/error/test", () => { throw new Exception("test"); });
+app.MapGet(
+    "/error/test",
+    () =>
+    {
+        throw new Exception("test");
+    }
+);
 
 // Controllers
 app.MapControllers();
